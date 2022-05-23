7e2dccfdb1613563cc5de08838a4d99439fb85f5
Jason Matta <jt.matta@gmail.com>
2022-05-23 14:20:24 -0700

    add product slogan and description

--- src/components/overview/Overview.js
+++ src/components/overview/Overview.js
@@ -1,6 +1,22 @@
-// import React from 'react';
+import { useState } from 'react';
+import ProductInfo from './overviewComponents/ProductInfo';
 
-function Overview() {
-  return <div>Hello World Overview!</div>
+export default function Overview({ product, productStyle }) {
+  const [productStyleId, setProductStyleId] = useState(0);
+  if(product.id && productStyle.product_id) {
+    return (
+      <div>
+        <ProductInfo
+          productCategory={product.category.toUpperCase()}
+          productTitle={product.name}
+          productOrginalPrice={productStyle.results[productStyleId].original_price}
+          productSalePrice={productStyle.results[productStyleId].sale_price}
+        />
+        <div>
+          <h4>{product.slogan}</h4>
+          <p>{product.description}</p>
+        </div>
+      </div>
+    )
+  }
 }
-export default Overview;
