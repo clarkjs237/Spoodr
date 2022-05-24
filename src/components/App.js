22dd917bf935907817b51f2d4bec42683501f5ed
Sully Clark <clarkjs237@gmail.com>
2022-05-24 10:02:27 -0700

    create global PRODUCT_ID and URL

--- src/components/App.js
+++ src/components/App.js
@@ -5,36 +5,38 @@ import Questions from './questions/Questions';
 import Related from './related/Related';
 import Reviews from './reviews/Reviews';
 
+const PRODUCT_ID = 65631;
+const URL = 'https://app-hrsei-api.herokuapp.com/api/fec2/rfp';
+
+// const Title = styled.h1`
+// font-size: 1.5em;
+// text-align: center;
+// color: palevioletred;
+// `;
+// const Wrapper = styled.section`
+// padding: 4em;
+// background: papayawhip;
+// `;
 
 function App() {
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-  `;
-  const Wrapper = styled.section`
-    padding: 4em;
-    background: papayawhip;
-  `;
-
-  const [product, setProduct] = useState(65631);
+  const [product, setProduct] = useState({'id': PRODUCT_ID});
   const [productStyle, setProductStyle] = useState({});
 
   function updateProductByID(id) {
-    fetch(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${id}`, {
+    fetch(`${URL}/products/${id}`, {
       headers: {
-        Authorization: process.env.GITTOKEN
-      }
+        Authorization: process.env.GITTOKEN,
+      },
     })
       .then((response) => response.json())
       .then((result) => setProduct(result))
       //remove catch later
       .catch((error) => console.log(error));
 
-    fetch(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${id}/styles`, {
+    fetch(`${URL}/products/${id}/styles`, {
       headers: {
-        Authorization: process.env.GITTOKEN
-      }
+        Authorization: process.env.GITTOKEN,
+      },
     })
       .then((response) => response.json())
       .then((result) => setProductStyle(result))
@@ -43,17 +45,17 @@ function App() {
   }
 
   useEffect(() => {
-    updateProductByID(product);
+    updateProductByID(product.id);
   }, []);
 
   return (
-    <Wrapper>
-      <Title>Hello World Title</Title>
+    <>
       <Overview product={product} productStyle={productStyle} />
       <Questions product={product} />
-      <Related product={product}/>
+      <Related product={product} />
       <Reviews product={product} />
-    </Wrapper>
+    </>
+
   );
 }
 
