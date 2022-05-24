3d4b0c80012b649aee26b7df6af1d7b726503d19
Jason Matta <jt.matta@gmail.com>
2022-05-24 03:03:56 -0700

    add socialmedia component

---
+++ src/components/overview/overviewComponents/SocialMedia.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import { FacebookShareButton, FacebookIcon, PinterestShareButton, PinterestIcon, TwitterShareButton, TwitterIcon } from 'react-share';
+
+const SocialMediaSpan = styled.span`
+  margin-left: ${props => props.wrap ? '1em' : '.5em'};
+`;
+
+export default function SocialMedia({ url, slogan }) {
+  return (
+    <SocialMediaSpan wrap>
+      <SocialMediaSpan>
+        <FacebookShareButton
+          url={url}
+          quote={slogan}>
+          <FacebookIcon size={20} round={true}/>
+        </FacebookShareButton>
+      </SocialMediaSpan>
+      <SocialMediaSpan>
+        <PinterestShareButton
+          media={url}
+          url={url}
+          description={slogan}>
+          <PinterestIcon size={20} round={true}/>
+        </PinterestShareButton>
+      </SocialMediaSpan>
+      <SocialMediaSpan>
+        <TwitterShareButton
+          url={url}
+          title={slogan}>
+          <TwitterIcon size={20} round={true}/>
+        </TwitterShareButton>
+      </SocialMediaSpan>
+    </SocialMediaSpan>
+  )
+}
