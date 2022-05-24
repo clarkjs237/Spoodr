a3a53ec3fae65081af90cc3e94406778d46135fc
Sully Clark <clarkjs237@gmail.com>
2022-05-24 16:45:52 -0700

    deleted comment blocks

--- src/components/related/RelatedList/RelatedListItem.js
+++ src/components/related/RelatedList/RelatedListItem.js
@@ -1,29 +1,10 @@
-// This will be responsible for actually making API requests to get both:
-// Styles
-// Ratings and Reviews so we can do that star thing
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { URL } from '../../App';
 
-// const Title = styled.h1`
-// font-size: 1.5em;
-// text-align: center;
-// color: palevioletred;
-// `;
 
 
 function RelatedListItem({ id, width }) {
-  // so this is where we will be making GET requests to get the product information
-  // namely, I want category, product name, price, rating, and pictures
-  // How do I decide which one will be shown?
-  // There are no pictures in the base model for the item, only in the styles. Should I just
-  // choose the first style?
-  // WE USE THE DEFAULT STYLE. THIS SHOULD PROBABLY BE THE FIRST ONE ONLY BUT CAN'T BE CERTAIN
-  // NEED TO LOOP THROUGH RESULTS AND SEE IF DEFAULT IS TRUE. IF IT IS THEN USE THIS AS THE INFO
-  // What to do if there is no default? It's still related but there is no default? How about just
-  // use the first item in the list?
-  // Another consideration is that the default style, even if it is the default, may have no
-  // pictures for it. This is troubling
   const [product, setProduct] = useState({});
 
   // Making a function to update the related IDs. This will be an array of ids
