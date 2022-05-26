2f52d0b084dc62829edd2e89663bda1a885e09f3
Jason Matta <jt.matta@gmail.com>
2022-05-25 18:03:05 -0700

    refactor gallary scroll and add unstable Overview.test

--- src/components/overview/overviewComponents/ImageGalleryDefaultComponents/DisplayImageNav.js
+++ src/components/overview/overviewComponents/ImageGalleryDefaultComponents/DisplayImageNav.js
@@ -5,7 +5,7 @@ const ImageNav = styled.div`
   position: absolute;
   top: 12rem;
   left: ${(props) => (props.next ? '35rem' : '3rem')};
-  font-size: 2rem;
+  font-size: 1.5rem;
   color: #32292F;
   &:hover {
     color: #90D7FF;
@@ -20,22 +20,20 @@ export default function DisplayImageNav({
 }) {
   function onClickHandler(e) {
     if (e.target.id === 'back') {
-      setCurDisplayIndex(curDisplayIndex - 1);
+      if(curDisplayIndex === 0) {
+        setCurDisplayIndex(maxDisplayIndex);
+      } else {
+        setCurDisplayIndex(curDisplayIndex - 1);
+      }
     } else {
-      setCurDisplayIndex(curDisplayIndex + 1);
+      if(curDisplayIndex === maxDisplayIndex) {
+        setCurDisplayIndex(0);
+      } else {
+        setCurDisplayIndex(curDisplayIndex + 1);
+      }
     }
   }
 
-  if (curDisplayIndex === 0) {
-    return (
-      <ImageNav next onClick={onClickHandler}>&#8594;</ImageNav>
-    );
-  }
-  if (curDisplayIndex === maxDisplayIndex) {
-    return (
-      <ImageNav id="back" onClick={onClickHandler}>&#8592;</ImageNav>
-    );
-  }
   return (
     <>
       <ImageNav id="back" onClick={onClickHandler}>&#8592;</ImageNav>
