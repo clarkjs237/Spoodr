bf9e82711b99dfaeacc33ca43265035d57aae6ea
Jason Matta <jt.matta@gmail.com>
2022-06-02 15:22:53 -0700

    refactor imagenav position for imagegalleries

--- src/components/overview/overviewComponents/ImageGalleryDefaultComponents/ThumbnailImageNav.js
+++ src/components/overview/overviewComponents/ImageGalleryDefaultComponents/ThumbnailImageNav.js
@@ -9,11 +9,11 @@ const NavArrow = styled.div`
   opacity: ${(props) => (props.clear ? '0' : '1')};
   height: auto;
   width: .1rem;
-  margin-left: ${(props) => (props.id === 'top' ? '1.8rem' : '1.45rem')};
+  margin-left: ${(props) => (props.id === 'top' ? '1.9rem' : '1.55rem')};
   border-width: 0;
   &:hover {
     color: #90D7FF;
-    cursor: pointer;
+    cursor: ${(props) => (props.clear ? 'zoom' : 'pointer')};
   }
 `;
 
